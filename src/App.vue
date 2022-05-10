f3bca7a8f50672b103333d8608a9a57f3a634201
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-05-10 19:17:02 +0200

    added-api

--- src/App.vue
+++ src/App.vue
@@ -8,30 +8,52 @@
 <script>
 import HeaderComponent from "./components/HeaderComponent.vue";
 import MainComponent from "./components/MainComponent.vue";
-import axios from axios;
+import axios from "axios";
 
 export default {
   name: "App",
+  props:{
+
+  },
   components: {
     HeaderComponent,
     MainComponent,
   },
+  data() {
+    return {
+      filmCards: {},
+      myApi: 'https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro'
+    };
+  },
+  mounted() {
+    axios
+      .get(this.myApi)
+      .then((res) => { 
+        console.log("ogg", res);
+        this.filmCards = res.data.results;
+      }) .catch((error) => {
+        console.log(error);
+      });
+
+      console.log("film",this.filmCards)
+  },
+  created(){
+
+
+  }
 };
-axios.get("")then((res)=>{
-  
-})
 </script>
 
-<style >
-*{
+<style>
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
-.debug{
+.debug {
   border: 1px solid black;
 }
-.container{
+.container {
   width: 90%;
   margin: 0 auto;
 }
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <header class="debug">
+      <div class="title">
+        <h1>Boolflix</h1>
+      </div>
+      <div class="serch-bar">
+        <input type="text" />
+        <button>Cerca</button>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+<style>
+header {
+  height: 80px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.title {
+  color: red;
+  text-transform: uppercase;
+
+}
+.serch-bar input, button{
+    height: 30px;
+
+}
+</style>
