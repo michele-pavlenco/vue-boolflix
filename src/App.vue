ba2e317eaf9763925df632dd2ed36ff4123dbb9c
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-05-10 16:12:51 +0200

    intestazione

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+    <MainComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+import MainComponent from "./components/MainComponent.vue";
+import axios from axios;
+
+export default {
+  name: "App",
+  components: {
+    HeaderComponent,
+    MainComponent,
+  },
+};
+axios.get("")then((res)=>{
+  
+})
+</script>
+
+<style >
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.debug{
+  border: 1px solid black;
+}
+.container{
+  width: 90%;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <main class="debug">
+      <ul>
+        <li></li>
+      </ul>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainComponent",
+};
+</script>
+
+<style scoped lang="scss">
+main {
+  height: calc(100vh - 80px);
+}
+</style>
